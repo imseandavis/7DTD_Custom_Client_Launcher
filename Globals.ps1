c1fe34344c21849472ab7e7a20c1363003a70879
Sean Davis <imseandavis@gmail.com>
2021-08-19 00:21:29 -0400

    Added Persistance To Check Boxes

--- Globals.ps1
+++ Globals.ps1
@@ -31,8 +31,30 @@ function Get-ScriptDirectory
 #Sample variable that provides the location of the script
 [string]$ScriptDirectory = Get-ScriptDirectory
 
-#Default Path Of 7 Days To Die Folder
+function Test-RegistryValue
+{
+	param (
+		[parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		$Path,
+		[parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		$Value
+	)
+	
+	try
+	{
+		Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
+		return $true
+	}
+	
+	catch
+	{
+		return $false
+	}
+}
 
+#Set Path Of 7 Days To Die Folder
 If (Test-Path -Path 'HKCU:\SOFTWARE\CindarLoader')
 {
 	$global:7DTDPath = Get-ItemPropertyValue 'HKCU:\Software\CindarLoader' -Name "GamePath"
@@ -44,4 +66,14 @@ else
 	$global:7DTDPath = Get-ItemPropertyValue 'HKCU:\Software\CindarLoader' -Name "GamePath"
 }
 
+# Create EAC and Cache Mode Registry Entries If They Don't Already Exist'
+If (!(Test-RegistryValue -Path 'HKCU:\Software\CindarLoader' -Value "EAC"))
+{
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\CindarLoader" -Name 'EAC' -Value "True" -Type "String"
+}
+
+If (!(Test-RegistryValue -Path 'HKCU:\Software\CindarLoader' -Value "CacheMode"))
+{
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\CindarLoader" -Name 'CacheMode' -Value "True" -Type "String"
+}
 	
